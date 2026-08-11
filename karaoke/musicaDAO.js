8e3d00afe95419e4e3c30d43750fe1697d1c4103
Évellyn Carvalho <evellyncarvalho1902@gmail.com>
2026-08-11 15:12:20 -0300

    atualizações

--- karaoke/musicaDAO.js
+++ karaoke/musicaDAO.js
@@ -1,12 +1,12 @@
 // DAO/MusicaDAO.js
-const { Musica } = require('../musica');
-const { Parte } = require('../parte');
+const { Musica } = require('./musica');
+const { Parte } = require('./parte');
 
 class MusicaDAO {
     constructor() {
         // "banco de dados" em memória — array de objetos Musica
         this.musicas = [];
-        this.proximoId = 0;
+        this.proximoId = 1;
     }
 
     // Retorna todas as músicas
@@ -71,7 +71,22 @@ constructor()
 
 _carregarDadosIniciais()
 {
-    const myHero = this.inserir('My Hero', 'Foo Fighters');
+    const Beatriz = this.inserir('Beatriz', '2ZDinizz');
     // Adicione as partes da My Hero usando myHero.addParte(...)
     // (mesmo código que estava no player.js)
+    beatriz.addParte(new Parte(intro, 3500, 'Intro'));
+    beatriz.addParte(new Parte(refrao1, 14000, 'Refrao1'));
+    beatriz.addParte(new Parte(verso1, 21000, 'Verso1'));
+    beatriz.addParte(new Parte(verso2, 21000, 'Verso2'));
+    beatriz.addParte(new Parte(verso3, 21000, 'Verso3'));
+    beatriz.addParte(new Parte(verso4, 21000, 'Verso4'));
+    beatriz.addParte(new Parte(ponte1, 12000, 'Ponte1'));
+    beatriz.addParte(new Parte(refrao2, 14000, 'Refrao2'));
+    beatriz.addParte(new Parte(verso5, 21000, 'Verso5'));
+    beatriz.addParte(new Parte(ponte2, 14000, 'Ponte2'));
+    beatriz.addParte(new Parte(verso6, 18000, 'Verso6'));
+    beatriz.addParte(new Parte(verso7, 18000, 'Verso7'));
+    beatriz.addParte(new Parte(refraoFinal, 14000, 'RefraoFinal'));
+    beatriz.addParte(new Parte(refraoFinal2, 14000, 'RefraoFinal2'));
+    beatriz.addParte(new Parte(final, 24000, 'Final'));
 }
